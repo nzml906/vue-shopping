f57e54f2c81202570d0f01ef539737854e52994c
Nazmul <grey000906@gmail.com>
2020-05-04 00:19:01 +0600

    deploy

---
+++ precache-manifest.c67d77d4785d18d84c5e582e6e745f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527b4ebad5f25a2d8bef",
+    "url": "/vue-shopping/css/app.0df0af7b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-shopping/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "72bd08f5b2e14538f7374291b0a250e1",
+    "url": "/vue-shopping/index.html"
+  },
+  {
+    "revision": "527b4ebad5f25a2d8bef",
+    "url": "/vue-shopping/js/app.6418f98a.js"
+  },
+  {
+    "revision": "e203a11e780a4a80a21d",
+    "url": "/vue-shopping/js/chunk-vendors.97cbbbc7.js"
+  },
+  {
+    "revision": "107fd6671f79ad0a8d9bd29d1f8e2967",
+    "url": "/vue-shopping/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-shopping/robots.txt"
+  }
+]);
